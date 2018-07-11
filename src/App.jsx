751598074a8a6af235fa335d00e91bcc02f39b7a
Mukund <mukundsharma@outlook.com>
2018-07-11 09:40:56 +0530

    Updated babel and app screenshot

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,29 @@ import './App.css';
 import 'antd/dist/antd.css';
 import logo from './images/futuretiplogo.png';
 
-class App extends Component {
+const Head = () => {
+  return (<header className="header">
+    <Row>
+      <Col lg={4} md={4}>
+        <img className="logo" src={logo} alt="futuretip logo">
+        </img>
+        <a href="#" className="logo-text">FutureTip</a>
+      </Col>
+      <Col lg={20} md={20}>
+        <ul className="main-nav">
+          <li><a href="#">Readings</a></li>
+          <li><a href="#">Testimonials</a></li>
+        </ul>
+      </Col>
+    </Row>
+  </header>)
+};
+
+class App extends React.Component {
   render () {
     return (
-      <header className="header">
-        <Row>
-          <Col lg={4} md={4}>
-            <img className="logo" src={logo} alt="futuretip logo">
-            </img>
-            <a href="#" className="logo-text">FutureTip</a>
-          </Col>
-          <Col lg={20} md={20}>
-            <ul className="main-nav">
-              <li><a href="#">Readings</a></li>
-              <li><a href="#">Testimonials</a></li>
-            </ul>
-          </Col>
-        </Row>
-      </header>
+      <Head/>
+
     )
   }
 }
