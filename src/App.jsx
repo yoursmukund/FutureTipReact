d43936c51cd0ef3bfa8b73d5e5a87311b6e5a018
Mukund <mukundsharma@outlook.com>
2018-07-12 11:45:08 +0530

    Added background and screenshot animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { Row, Col } from 'antd';
 import './App.css';
 import 'antd/dist/antd.css';
 import logo from './images/futuretiplogo.png';
+import screenshot from './images/screenshot1.png';
 
-const Head = () => {
+const Header = () => {
   return (<header className="header">
     <Row>
       <Col lg={4} md={4}>
@@ -22,11 +23,36 @@ const Head = () => {
   </header>)
 };
 
+const AppDisplay = () => {
+  return (
+    <Row>
+      <Col className="app-container">
+        <Col lg={12} md={12}>
+          <img className="app-screenshot" src={screenshot} alt="app screenshot"/>
+        </Col>
+        <Col lg={12} md={12} className="tagline-container">
+          <div className='tagline'>
+            <h1>Meet your new</h1>
+            <h2>Astrologer</h2>
+            <h2>+</h2>
+            <h2>Therapist</h2>
+          </div>
+        </Col>
+      </Col>
+    </Row>
+  )
+}
+
 class App extends React.Component {
   render () {
     return (
-      <Head/>
-
+      <div>
+        <div class="background-image"></div>
+        <div class="container">
+          <Header/>
+          <AppDisplay/>
+        </div>
+      </div>
     )
   }
 }
