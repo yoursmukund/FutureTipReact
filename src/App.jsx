96fc7156448ec179b6a6923915d53f2084079132
Mukund <mukundsharma@outlook.com>
2018-07-22 10:52:06 +0530

    RWD changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Icon } from 'antd';
 import './App.css';
+import './media-queries.css';
 import 'antd/dist/antd.css';
 import logo from './images/futuretiplogo.png';
 import screenshot from './images/screenshot1.png';
@@ -8,16 +9,17 @@ import screenshot from './images/screenshot1.png';
 const Header = () => {
   return (<header className="header">
     <Row>
-      <Col lg={4} md={4}>
+      <Col xl={8} lg={8} md={8} sm={8} xs={15}>
         <img className="logo" src={logo} alt="futuretip logo">
         </img>
         <a href="#" className="logo-text">FutureTip</a>
       </Col>
-      <Col lg={20} md={20}>
+      <Col xl={16} lg={16} md={16} sm={16} xs={9}>
         <ul className="main-nav">
           <li><a href="#">Readings</a></li>
           <li><a href="#">Testimonials</a></li>
         </ul>
+        <i className="fas fa-bars bars-icon"></i>
       </Col>
     </Row>
   </header>)
@@ -27,12 +29,12 @@ const AppDisplay = () => {
   return (
     <Row>
       <Col className="app-container">
-        <Col lg={12} md={12}>
+        <Col lg={12} md={12} md={12} sm={12} xs={14}>
           <img className="app-screenshot" src={screenshot} alt="app screenshot"/>
         </Col>
-        <Col lg={12} md={12} className="tagline-container">
+        <Col lg={12} md={12} md={12} sm={12} xs={10} className="tagline-container">
           <div className='tagline'>
-            <h1>Meet your personal</h1>
+            <p>Meet your personal</p>
             <h2>Astrologer</h2>
             <h2>+</h2>
             <h2>Therapist</h2>
